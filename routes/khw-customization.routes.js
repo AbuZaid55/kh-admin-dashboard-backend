436b04ad63f64a065981dfe3078b50f727cf2f89
Awdhesh9211 <gaundawdhesh9211@gmail.com>
2025-03-09 09:00:54 +0530

    added khwaahish homepage API

---
+++ routes/khw-customization.routes.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const { 
+  getHomepage, updateGeneral, updateHallMark, addHighJewel, addHighJewelCarousal,
+  deleteHighJewelCarousal, QueenOfHeartSection, addQOHCarousal, deleteQOHCarousal,
+  updateEditSection, promiseSection, addPromise, deletePromise,
+  updateComingSoon, updateStoreSection, addStoreFeat, deleteStoreFeat, faqSection,
+  addFaq, deleteFaq, toggleSection
+} = require("../controllers/khwaahish/homepage.controller.js");
+const { uploads } = require('../services/S3_Services.js');
+
+// KHWAAHISH HOMEPAGE 
+router.get("/", getHomepage);
+
+router.put("/general", uploads.fields([{ name: "logo" }, { name: "hero_video" }]), updateGeneral);
+
+router.put("/hallmark", uploads.fields([{name:"hallmark_images"}]), updateHallMark);
+
+router.post("/high-jewel", addHighJewel);
+router.post("/high-jewel-carousel", uploads.single("high_Image"), addHighJewelCarousal);
+router.delete("/high-jewel-carousel/:id", deleteHighJewelCarousal);
+
+router.put("/qoh", QueenOfHeartSection);
+router.post("/qoh-carousel", uploads.single("qohImage"), addQOHCarousal);
+router.delete("/qoh-carousel/:id", deleteQOHCarousal);
+
+router.put("/edit", uploads.fields([{ name: "bridal_edit_image" }, { name: "polki_edit_image" }]), updateEditSection);
+
+router.put("/promise", promiseSection);
+router.post("/promise", uploads.single("promise_image"), addPromise);
+router.delete("/promise/:id", deletePromise);
+
+router.put("/coming-soon", uploads.fields([{ name: "coming_soon_images" }, { name: "coming_soon_videCall_banner_image" }]), updateComingSoon);
+
+router.put("/store", uploads.single("storeImage"), updateStoreSection);
+router.post("/store-feature", addStoreFeat);
+router.delete("/store-feature/:id", deleteStoreFeat);
+
+router.put("/faq", faqSection);
+router.post("/faq", addFaq);
+router.delete("/faq/:id", deleteFaq);
+
+router.put("/toggle-section", toggleSection);
+
+
+
+module.exports = router;
