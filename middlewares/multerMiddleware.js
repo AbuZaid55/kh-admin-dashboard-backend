aa48237668e69f47b0636709cc6ee069e1882c13
AbuZaid55 <zaid70979@gmail.com>
2025-03-10 14:24:11 +0530

    added: tmp folder

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/excel/");
+    cb(null, "tmp/excel/");
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
