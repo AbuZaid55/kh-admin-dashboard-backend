d5409b991fe706d17eda0aa9b40d8fc2ea3555c9
Anup Suresh <anupsuresh216@gmail.com>
2025-03-17 17:57:49 +0530

    testing

--- controllers/khwaahish/khw-testimonial.controller.js
+++ controllers/khwaahish/khw-testimonial.controller.js
@@ -5,13 +5,23 @@ const { deleteFileFromS3 } = require("../../services/S3_Services"); // Adjust pa
 exports.createTestimonial = async (req, res) => {
   try {
     const { name, designation, testimonial, collection_id } = req.body;
-
     // S3 storage
-    const profile_img = req.file ? { key: req.file.key, url: req.file.location } : null;
+    const profile_img = req.file
+      ? { key: req.file.key, url: req.file.location }
+      : null;
+    // Ensure collection_id is valid
+    if (!collection_id) {
+      return res.status(400).json({ message: "Collection ID is required" });
+    }
+    console.log(collection_id);
+    const newTestimonial = await Testimonial.create({
+      name,
+      designation,
+      testimonial,
+      khw_collection_id: collection_id,
+      profile_img,
+    });
 
-    const newTestimonial = new Testimonial({ name, designation, testimonial, profile_img, collection_id });
-    await newTestimonial.save();
-    
     res.status(201).json(newTestimonial);
   } catch (error) {
     res.status(500).json({ message: "Error creating testimonial", error });
@@ -24,7 +34,7 @@ exports.getTestimonials = async (req, res) => {
     const { collection_id } = req.query;
     const filter = collection_id ? { collection_id } : {}; // If collection_id is provided, filter by it
     const testimonials = await Testimonial.find(filter);
-    
+
     res.status(200).json(testimonials);
   } catch (error) {
     res.status(500).json({ message: "Error fetching testimonials", error });
@@ -42,9 +52,14 @@ exports.updateTestimonial = async (req, res) => {
       updatedData.profile_img = { key: req.file.key, url: req.file.location };
     }
 
-    const updatedTestimonial = await Testimonial.findByIdAndUpdate(req.params.id, updatedData, { new: true });
+    const updatedTestimonial = await Testimonial.findByIdAndUpdate(
+      req.params.id,
+      updatedData,
+      { new: true }
+    );
 
-    if (!updatedTestimonial) return res.status(404).json({ message: "Testimonial not found" });
+    if (!updatedTestimonial)
+      return res.status(404).json({ message: "Testimonial not found" });
 
     res.status(200).json(updatedTestimonial);
   } catch (error) {
@@ -56,7 +71,8 @@ exports.updateTestimonial = async (req, res) => {
 exports.deleteTestimonial = async (req, res) => {
   try {
     const testimonial = await Testimonial.findById(req.params.id);
-    if (!testimonial) return res.status(404).json({ message: "Testimonial not found" });
+    if (!testimonial)
+      return res.status(404).json({ message: "Testimonial not found" });
 
     // If using S3 storage, delete the file from S3
     if (testimonial.profile_img?.key) {
